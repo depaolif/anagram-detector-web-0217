47ce7655177762aa754d1bc74c847a218fd3355e
Frederico De Paoli <frederico.de.paoli@gmail.com>
2017-02-08 16:27:00 -0500

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,22 @@
 # Your code goes here!
+class Anagram
+	attr_accessor :word
+	def initialize(word)
+		@word = word
+	end
+	
+	def match(array)
+		matches = []
+		array.each do |word|
+			if anagram_check(word)
+				matches << word
+			end
+		end
+		matches
+	end
+
+	def anagram_check(word)
+		@word.split("").sort == word.split("").sort
+	end
+	
+end
